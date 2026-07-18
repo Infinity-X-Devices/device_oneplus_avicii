5ecf14e468a2a3c983226f0f828dba06c2e6b383
sreeshankark <sreeshankar0910@gmail.com>
2026-07-18 11:57:08 +0530

    avicii: Update screen density

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ $(call soong_config_set,camera,package_name,com.oneplus.camera)
 
 # Display
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
-TARGET_SCREEN_DENSITY := 399
+TARGET_SCREEN_DENSITY := 450
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
